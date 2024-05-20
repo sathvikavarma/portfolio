60e9eec9e8eee744d7a7a47b9001b22313140205
Anurakti <anuraktiarora92@gmail.com>
2024-05-20 01:05:09 +0100

    updated home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
           Anurakti Arora
         </h1>
         <h2 className="text-4xl sm:text-6xl font-bold text-[#8892b0] my-2">
-          I'm a Software Engineer.
+          I'm a UI Developer.
         </h2>
         <div>
           <Link to="work" smooth={true} duration={500}>
