45f5320f8ef1ad990a897c2f6faaa14a4f636263
sathvikavarma <sathvika.chekuri@gmail.com>
2025-08-16 17:04:09 +0530

    fixed lint

--- src/components/Work.js
+++ src/components/Work.js
@@ -53,12 +53,12 @@ const Work = () => {
                 Disney Clone
               </span>
               <div className="pt-8 text-center">
-                <a>
+                <a href="#">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a>
+                <a href="#">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -81,7 +81,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a>
+                <a href="#">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
