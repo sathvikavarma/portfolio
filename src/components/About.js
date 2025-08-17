f8a9c34d7611eaf3208009005f8efa7f32b80fd1
sathvikavarma <sathvika.chekuri@gmail.com>
2025-08-17 16:21:32 +0530

    fixed css for device view

--- src/components/About.js
+++ src/components/About.js
@@ -2,36 +2,30 @@ import photo from "../assets/coder.png";
 
 const About = () => {
   return (
-    <div name="about" className=" w-full h-screen text-gray-300">
-      <div className="flex flex-row justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2">
-          <div className="">
-            <img className="rounded-full w-96" src={photo} alt="" />
-          </div>
-          <div className="">
-            <p className=" text-4xl text-black font-bold inline border-b-4 border-pink-600">
-              About
-            </p>
-           <p className="mt-6 text-white">
-  Hi👋 &nbsp;I'm Sathvika Chekuri, nice to meet you.
-</p>
-<br />
-<p className="text-white">
-  <br />
-  Hey! I'm a full-stack developer who loves building efficient, scalable, and intuitive applications. 
-  I thrive on solving complex problems, optimizing performance, and creating seamless user experiences. 
-  I have 3 years of experience as a Software Engineer in the IT
-  industry.
-  <br />
-  You can see some of my projects and the skills that I have
-  acquired in my journey as a Software Engineer.
-</p>
-<br />
-<p className="text-white">
-  Currently, I am looking out for a job<br />Please
-  feel free to contact me at sathvika.chekuri@gmail.com
-</p>
-          </div>
+    <div name="about" className="w-full min-h-screen text-gray-300 px-4 py-8">
+      <div className="max-w-[1000px] mx-auto flex flex-col md:grid md:grid-cols-2 items-center gap-8 mt-32">
+        {/* Image Section */}
+        <div className="flex justify-center">
+          <img className="rounded-full w-64 md:w-96" src={photo} alt="Coder" />
+        </div>
+        {/* Text Section */}
+        <div>
+          <p className="text-4xl text-black font-bold inline border-b-4 border-pink-600">
+            About
+          </p>
+          <p className="mt-6 text-white text-lg">
+            Hi 👋 I'm <span className="font-semibold text-pink-400">Sathvika Chekuri</span>, nice to meet you.
+          </p>
+          <p className="mt-4 text-white text-base leading-relaxed">
+            I'm a full-stack developer who loves building efficient, scalable, and intuitive applications. I thrive on solving complex problems, optimizing performance, and creating seamless user experiences. With 3 years of experience in the IT industry, I've worked on diverse projects that showcase my skills and passion for development.
+          </p>
+          <p className="mt-4 text-white text-base">
+            You can explore some of my projects and the skills I've acquired throughout my journey as a Software Engineer.
+          </p>
+          <p className="mt-4 text-white text-base">
+            <span className="font-semibold text-pink-400">Currently open to opportunities!</span> <br />
+            Feel free to reach out at <a href="mailto:sathvika.chekuri@gmail.com" className="underline text-pink-400">sathvika.chekuri@gmail.com</a>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,97 +4,100 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import { Link } from "react-scroll";
 
-
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[64px] flex flex-col items-center justify-center px-4 text-gray-300 bg-transparent z-50">
-   <ul className="hidden md:flex justify-center items-center space-x-8 -mt-2">
-    <li>
-      <Link to="home" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
-        Home
-      </Link>
-    </li>
-    <li>
-      <Link to="about" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
-        About
-      </Link>
-    </li>
-    <li>
-      <Link to="skills" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
-        Skills
-      </Link>
-    </li>
-    <li>
-      <Link to="work" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
-        Work
-      </Link>
-    </li>
-    <li>
-      <Link to="contact" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
-        Contact
-      </Link>
-    </li>
-  </ul>
-      {/* hamburger */}
-      <div onClick={handleClick} className="md:hidden z-10 py-6 ">
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-      {/* mobile menu  */}
-      <ul
-        className={
-          !nav
-            ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center"
-        }
-      >
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+      {/* Desktop Menu */}
+      <ul className="hidden md:flex justify-center items-center space-x-8">
+        <li>
+          <Link to="home" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+        <li>
+          <Link to="about" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+        <li>
+          <Link to="skills" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
             Skills
           </Link>
         </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+        <li>
+          <Link to="work" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
             Work
           </Link>
         </li>
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+        <li>
+          <Link to="contact" smooth={true} duration={500} className="text-xl font-bold text-blue-700">
             Contact
           </Link>
         </li>
       </ul>
-      {/* scoial icons */}
-      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+      {/* Hamburger Icon */}
+      <div onClick={handleClick} className="md:hidden z-50 py-6">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+  className={
+    !nav
+      ? "hidden"
+      : "fixed top-0 left-0 w-full min-h-screen flex flex-col justify-center items-center bg-[#0a192f] z-50"
+  }
+>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500} className="text-white">
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500} className="text-white">
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500} className="text-white">
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500} className="text-white">
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500} className="text-white">
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Social Icons (Desktop Only) */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0 z-40">
         <ul>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-30 bg-blue-500">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="https://www.linkedin.com/in/sathvika-chekuri-78161419b/"
+              target="_blank" rel="noopener noreferrer"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-30 bg-[#333333]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="https://github.com/sathvikavarma"
+              target="_blank" rel="noopener noreferrer"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-30 bg-[#6fc2b0]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="mailto:sathvika.chekuri@gmail.com"
@@ -102,10 +105,11 @@ const Navbar = () => {
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-30 bg-[#565f69]">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
               href="/Sathvika_resume.pdf"
+              target="_blank" rel="noopener noreferrer"
             >
               CV <BsFillPersonLinesFill size={30} />
             </a>
@@ -115,4 +119,5 @@ const Navbar = () => {
     </div>
   );
 };
+
 export default Navbar;
